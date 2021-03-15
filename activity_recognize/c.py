ac7d664d6ba4df51ff88f57ed9846186f1ba0f0b
tianzhongwei <tianzhongwei@xiaomi.com>
2021-03-15 18:35:09 +0800

    add c.py into master

---
+++ activity_recognize/c.py
@@ -0,0 +1,7 @@
+'''
+Author       : Tianzw
+Date         : 2021-03-15 18:34:39
+LastEditors  : Please set LastEditors
+LastEditTime : 2021-03-15 18:34:40
+FilePath     : /my_github/activity_recognize/c.py
+'''
