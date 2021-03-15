1c185eb558e0f1aa0f68d3fdcda9b159aa344bdc
tianzhongwei <tianzhongwei@xiaomi.com>
2021-03-15 18:33:41 +0800

    add a.py b.py into develop

---
+++ activity_recognize/a.py
@@ -0,0 +1,11 @@
+'''
+Author       : Tianzw
+Date         : 2021-03-15 18:31:39
+LastEditors  : Please set LastEditors
+LastEditTime : 2021-03-15 18:31:55
+FilePath     : /my_github/activity_recognize/a.py
+'''
+
+
+def main():
+    print('a')
---
+++ activity_recognize/b.py
@@ -0,0 +1,7 @@
+'''
+Author       : Tianzw
+Date         : 2021-03-15 18:32:03
+LastEditors  : Please set LastEditors
+LastEditTime : 2021-03-15 18:32:04
+FilePath     : /my_github/activity_recognize/b.py
+'''
