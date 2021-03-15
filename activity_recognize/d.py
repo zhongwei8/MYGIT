488498d6cdc5e1203a6a813761b70db778c7313c
tianzhongwei <tianzhongwei@xiaomi.com>
2021-03-15 18:35:41 +0800

    add d.py into master

---
+++ activity_recognize/d.py
@@ -0,0 +1,7 @@
+'''
+Author       : Tianzw
+Date         : 2021-03-15 18:35:24
+LastEditors  : Please set LastEditors
+LastEditTime : 2021-03-15 18:35:25
+FilePath     : /my_github/activity_recognize/d.py
+'''
